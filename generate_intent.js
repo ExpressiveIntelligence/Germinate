c3b4f3160bf5677d795bcd44dd4aefb0acb8b632
Max Kreminski <maxkreminski@gmail.com>
2019-11-07 18:42:28 -0800

    Significantly improve ASP intent generation code
    * Never generate triple-negative statements ("prevent not not")
    * Correctly calculate values for {min,max}_{entities,resources} declarations
    * Randomize unspecified icons at intent generation time
    * Improve header comment generation for entities and resources, esp. nameless ones
    * Swap codegen for neutral and complicated valence tags (they were backwards before)
    * Temporarily omit negated neutral/complicated valence tags from codegen
    * Substantially clean up relationship codegen, esp. for error cases

--- generate_intent.js
+++ generate_intent.js
@@ -10,32 +10,41 @@ function findThingWithName(intent, name) {
 }
 
 function generateASPForValenceTag(thingASPHandle, tag) {
+  // TODO if negated good, bad and neutral tags are all present on the same thing,
+  // we generate an unsatisfiable intent
   let asp = '';
   if (tag.value === 'good') {
     asp += `:- not reading(good,${thingASPHandle}). % positive valence\n`;
     if (tag.isNegated) {
-      asp = asp.replace(':- not', ':- not not');
+      asp = asp.replace(':- not', ':-').replace('%', '% NOT');
     }
   } else if (tag.value === 'bad') {
     asp += `:- not reading(bad,${thingASPHandle}). % negative valence\n`;
     if (tag.isNegated) {
-      asp = asp.replace(':- not', ':- not not');
+      asp = asp.replace(':- not', ':-').replace('%', '% NOT');
     }
   } else if (tag.value === 'neutral') {
-    // TODO negation is weird here so we're just ignoring it for now
-    // (negated 'neutral' is just good or bad or complicated)
-    asp += `:- not reading(good,${thingASPHandle}). % neutral valence\n`;
-    asp += `:- not reading(bad,${thingASPHandle}).  % neutral valence\n`;
+    if (tag.isNegated) {
+      // TODO negation is weird here so we're just ignoring it for now
+      // (negated 'neutral' should become "1 { good, bad } 2")
+      console.warn("can't currently generate ASP for negated 'neutral' valence tag!", tag);
+      asp += "% (omitted negated 'neutral' valence tag)\n";
+    } else {
+      asp += `:- reading(good,${thingASPHandle}). % neutral valence\n`;
+      asp += `:- reading(bad,${thingASPHandle}).  % neutral valence\n`;
+    }
   } else if (tag.value === 'complicated') {
-    // TODO negation is weird here so we're just ignoring it for now
-    // (negated 'complicated' is just good or bad or neutral)
-    asp += `:- not not reading(good,${thingASPHandle}). % complicated valence\n`;
-    asp += `:- not not reading(bad,${thingASPHandle}).  % complicated valence\n`;
+    if (tag.isNegated) {
+      // TODO negation is weird here so we're just ignoring it for now
+      // (negated 'complicated' should become "0 { good, bad } 1")
+      console.warn("can't currently generate ASP for negated 'complicated' valence tag!", tag);
+      asp += "% (omitted negated 'complicated' valence tag)\n";
+    } else {
+      asp += `:- not reading(good,${thingASPHandle}). % complicated valence\n`;
+      asp += `:- not reading(bad,${thingASPHandle}).  % complicated valence\n`;
+    }
   } else {
-    console.warn('invalid valence tag value!', valenceTags[0]);
-  }
-  if (tag.isNegated) {
-    asp = asp.replace(/%/g, '% negated');
+    console.warn('invalid valence tag value!', tag);
   }
   return asp;
 }
@@ -57,11 +66,13 @@ function generateASPForValueTag(thingASPHandle, tag) {
 
 function generateASPForEntity(entity) {
   // TODO handle optional entities
-  let thingASPHandle = makeThingASPHandle(entity);
-  let asp = `% ENTITY: ${entity.name}\n`;
+  const thingASPHandle = makeThingASPHandle(entity);
+  const thingName = entity.name === '' ? '$$RANDOM_NAME$$' : entity.name;
+  let asp = `% ENTITY: ${thingName} (${entity.id})\n`;
   // label is actually just emoji icon for now
-  // TODO if icon is the qmark one, swap it for a random one from the current theme (or the default theme if none)
-  asp += `label(${thingASPHandle},${entity.icon},write).\n`;
+  // if icon is the question mark, swap it for a random one from the current theme
+  const icon = entity.icon === '❓' ? randNth(getCurrentTheme().icons) : entity.icon;
+  asp += `label(${thingASPHandle},${icon},write).\n`;
   // set its valence (good, bad, whatever)
   let valenceTags = entity.tags.filter(t => t.family === 'playerAttitude');
   for (let valenceTag of valenceTags) {
@@ -73,9 +84,10 @@ function generateASPForEntity(entity) {
 
 function generateASPForResource(resource) {
   // TODO handle optional resources
-  let thingASPHandle = makeThingASPHandle(resource);
-  let asp = `% RESOURCE: ${resource.name}\n`;
-  asp += `label(${thingASPHandle},${resource.name},write).\n`;
+  const thingASPHandle = makeThingASPHandle(resource);
+  const thingName = resource.name === '' ? '$$RANDOM_NAME$$' : resource.name;
+  let asp = `% RESOURCE: ${thingName} (${resource.id})\n`;
+  asp += `label(${thingASPHandle},${thingName},write).\n`;
   // set its valence (good, bad, whatever)
   let valenceTags = resource.tags.filter(t => t.family === 'playerAttitude');
   for (let valenceTag of valenceTags) {
@@ -92,29 +104,50 @@ function generateASPForResource(resource) {
 function generateASPForRelationship(intent, relationship) {
   // TODO handle negated relationships
   // TODO check if it makes sense for this kind of relationship to exist between these two things
-  // (persecute non-conforming relationships)
-  // TODO complain to the user if the named LHS and RHS things don't actually exist?
-  // (but still allow empty string to denote "anything"?)
-  let lhsThing = findThingWithName(intent, relationship.lhs);
-  let rhsThing = findThingWithName(intent, relationship.rhs);
-  let lhsThingName = lhsThing ? lhsThing.name : '_';
-  let rhsThingName = rhsThing ? rhsThing.name : '_';
-  let lhsThingASPHandle = lhsThing ? makeThingASPHandle(lhsThing) : '_';
-  let rhsThingASPHandle = rhsThing ? makeThingASPHandle(rhsThing) : '_';
-  let asp = `% RELATIONSHIP: ${lhsThingName} ${relationship.reltype} ${rhsThingName}\n`;
-  if (relationship.reltype === 'consumes') {
-    asp += `:- not reading(consumes,relation(${lhsThingASPHandle},${rhsThingASPHandle})).`;
-  } else if (relationship.reltype === 'produces') {
-    asp += `:- not reading(produces,relation(${lhsThingASPHandle},${rhsThingASPHandle})).`;
-  } else if (relationship.reltype === 'collides with') {
-    asp += `% apply restitution between ${lhsThingName} and ${rhsThingName} every frame
+  // TODO complain to the user if the named LHS and RHS things don't actually exist
+  const {lhs, rhs, reltype} = relationship;
+
+  // figure out whether lhs and rhs are deliberately wildcards
+  const lhsIsWildcard = lhs === '';
+  const rhsIsWildcard = rhs === '';
+
+  // generate header comment
+  let asp = `% RELATIONSHIP: ${lhsIsWildcard ? '_' : lhs} ${reltype} ${rhsIsWildcard ? '_' : rhs}\n`;
+
+  // bail out early if lhs or rhs are non-wildcard but don't point to any extant thing
+  const lhsThing = lhsIsWildcard ? null : findThingWithName(intent, lhs);
+  const rhsThing = rhsIsWildcard ? null : findThingWithName(intent, rhs);
+  if (!lhsThing && !lhsIsWildcard) {
+    console.warn(`couldn't find an entity or resource named ${lhs}!`);
+    asp += `% (omitted code for relationship involving nonexistent entity or resource type ${lhs})\n\n`;
+    return asp;
+  }
+  if (!rhsThing && !rhsIsWildcard) {
+    console.warn(`couldn't find an entity or resource named ${rhs}!`);
+    asp += `% (omitted code for relationship involving nonexistent entity or resource type ${rhs})\n\n`;
+    return asp;
+  }
+
+  // generate appropriate ASP for the specified reltype
+  const lhsASPHandle = lhsThing ? makeThingASPHandle(lhsThing) : '_';
+  const rhsASPHandle = rhsThing ? makeThingASPHandle(rhsThing) : '_';
+  if (reltype === 'consumes') {
+    asp += `:- not reading(consumes,relation(${lhsASPHandle},${rhsASPHandle})).`;
+  } else if (reltype === 'produces') {
+    asp += `:- not reading(produces,relation(${lhsASPHandle},${rhsASPHandle})).`;
+  } else if (reltype === 'collides with') {
+    asp += `% apply restitution between these entities every frame
 precondition(tick,tick).
-result(tick,apply_restitution(${lhsThingASPHandle},${rhsThingASPHandle})).`;
+result(tick,apply_restitution(${lhsASPHandle},${rhsASPHandle})).`;
   } else {
-    console.warn(`I ain't never heard of no relationship with reltype: ${relationship.reltype} before!
+    console.warn(`I ain't never heard of no relationship with reltype: ${reltype} before! \
 What in tarnation are you doin'?`);
   }
   return asp + '\n\n';
+}
+
+function isThingRequired(thing) {
+  return thing.tags.filter(tag => tag.family === 'optional').length === 0;
 }
 
 function generateASPForIntent(intent) {
@@ -124,14 +157,30 @@ function generateASPForIntent(intent) {
   let asp = `% generated at ${Date.now()}\n\n`;
   // TODO rest of the universal preamble
 
-  // TODO figure out how many entity slots we'll need, set min and max entities
-  // TODO same for resources
+  // figure out how many entity slots we'll need, set min and max entities
+  const entities = Object.values(intent).filter(t => t.type === 'entity');
+  const minEntitySlots = entities.filter(isThingRequired).length;
+  const maxEntitySlots = entities.length;
+  asp += `#const min_entities = ${minEntitySlots}.
+#const max_entities = ${maxEntitySlots}.\n`;
+
+  // figure out how many resource slots we'll need, set min and max resources
+  const resources = Object.values(intent).filter(t => t.type === 'resource');
+  const minResourceSlots = resources.filter(isThingRequired).length;
+  const maxResourceSlots = resources.length;
+  asp += `#const min_resources = ${minResourceSlots}.
+#const max_resources = ${maxResourceSlots}.\n`;
+
+  // set min and max outcomes, assuming these are always constants
+  asp += `#const min_outcomes = 2.
+#const max_outcomes = 10.
+#const min_end_outcomes = 0.\n\n`;
 
   // intent has: entities, resources, relationships, triggers
-  for (let entity of Object.values(intent).filter(t => t.type === 'entity')) {
+  for (let entity of entities) {
     asp += generateASPForEntity(entity);
   }
-  for (let resource of Object.values(intent).filter(t => t.type === 'resource')) {
+  for (let resource of resources) {
     asp += generateASPForResource(resource);
   }
   for (let relationship of Object.values(intent).filter(t => t.type === 'relationship')) {
